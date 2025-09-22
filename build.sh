0ee52a78b00457c9f998795c9016a2e668978d73
Joshua Garde <jgarde@jpl.nasa.gov>
2025-09-22 11:49:57 -0700

    Fix build script permissions + path issue

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 VERSION=$(<"$(dirname $BASH_SOURCE)/VERSION")
 
 mkdir -p "$(dirname $BASH_SOURCE)/build"
-rm -f ../build/carpathia-lambdas-${VERSION}.zip
+rm -f "$(dirname $BASH_SOURCE)/build/carpathia-lambdas-${VERSION}.zip"
 cd "$(dirname $BASH_SOURCE)/lambdas"
-zip -r9 ../build/carpathia-lambdas-${VERSION}.zip .
+zip -r9 "../build/carpathia-lambdas-${VERSION}.zip" .
