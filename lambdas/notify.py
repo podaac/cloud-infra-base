b1a58764594da6705fe26dcae53e1cdc8621ff40
Joshua Garde <jgarde@jpl.nasa.gov>
2025-09-22 11:50:10 -0700

    Update to SNS event delivery

--- lambdas/notify.py
+++ lambdas/notify.py
@@ -3,43 +3,47 @@ import boto3
 from os import environ
 import json
 import logging
+import re
 
 logging.basicConfig(level=logging.INFO)
+SNS_TOPIC_ARN = environ.get("SNS_TOPIC_ARN")
+
 ec2_client = boto3.client("ec2")
 ssm_client = boto3.client("ssm")
 sns_client = boto3.client("sns")
 lambda_client = boto3.client("lambda")
 
-SNS_TOPIC_ARN = environ.get("SNS_TOPIC_ARN")
+status_check_fail_re = re.compile(r'taken out of service in response to an EC2 instance status checks failure\.$')
 
 
-def lambda_handler(event,context):
-    if 'Service' not in event or event['Service'] != 'AWS Auto Scaling':
-        logging.warning("Ignoring event from unsupported service")
-        return
-
-    logging.info("Processing event: %s", json.dumps(event))
-    if event['LifecycleTransition'] != 'autoscaling:EC2_INSTANCE_TERMINATING':
-        logging.warning("Ignoring event from unsupported lifecycle transition")
-        return
-
-    instance_statuses = ec2_client.describe_instance_status(
-        InstanceIds=[event['EC2InstanceId']],
-        IncludeAllInstances=True
-    )
-    
+def lambda_handler(event, context):
     tags = lambda_client.list_tags(
         Resource=context.invoked_function_arn
     )['Tags']
 
-    health_status = instance_statuses['InstanceStatuses'][0]['InstanceStatus']['Status']
+    environment = tags.get("Environment", "Unknown Environment")
 
-    if health_status == 'impaired':
-        logging.info('Instance %s was terminated because it was unhealthy', event['EC2InstanceId'])
-        logging.info('Sending notification to SNS topic: %s', SNS_TOPIC_ARN)
+    for record in event.get("Records"):
+        event = json.loads(record.get("Sns", {}).get("Message", "{}"))
+        process_message(event, environment)
+
+
+def process_message(message, environment):
+    if 'Service' not in message or message['Service'] != 'AWS Auto Scaling':
+        logging.warning("Ignoring event from unsupported service")
+        return
+
+    print("Processing event: %s", json.dumps(message))
+    if message['Event'] != 'autoscaling:EC2_INSTANCE_TERMINATE':
+        logging.warning(f"Ignoring unsupported event {message['Event']}")
+        return
+
+    if status_check_fail_re.search(message['Cause']):
+        logging.info(f'Instance {message["EC2InstanceId"]} was terminated because it was unhealthy')
+        logging.info(f'Sending notification to SNS topic: {SNS_TOPIC_ARN}')
 
         sns_client.publish(
             TopicArn=SNS_TOPIC_ARN,
-            Subject=f'[{tags.get("Environment", "Unknown Environment")}] Unhealthy Instance Termination',
-            Message=f'Carpathia instance {event["EC2InstanceId"]} in {tags.get("Environment", "Unknown Environment")} was terminated due to health check failure.'
+            Subject=f'[{environment}] Unhealthy Instance Termination',
+            Message=f'Carpathia instance {message["EC2InstanceId"]} in {environment} was terminated due to health check failure.'
         )
